f105e7d061e687344af09d22522f22f0a3d939ba
Anderson Rodriguez <andersoncamilo.rodriguez.s@gmail.com>
2025-01-20 21:24:35 -0500

    Archivos añadidos

---
+++ src/test/java/com/anderson/forhub/ForhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.forhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
