56bb42524073ee0c55a0cacd4d2b5d93aa6011ce
Anderson Rodriguez <andersoncamilo.rodriguez.s@gmail.com>
2025-01-21 19:41:54 -0500

    Crea el DTO para mostrar los topicos

---
+++ src/main/java/com/anderson/forohub/domain/topico/MostrarDatosTopico.java
@@ -0,0 +1,10 @@
+package com.anderson.forohub.domain.topico;
+
+public record MostrarDatosTopico(
+        Long id,
+        String titulo,
+        String nombreCurso,
+        String mensaje,
+        String motivo
+) {
+}
