0bb7ae11d3adafaa3ac50dca0a5ab02696a03942
Anderson Rodriguez <andersoncamilo.rodriguez.s@gmail.com>
2025-01-24 00:02:45 -0500

    Agrega restriccion de unicidad en la columna nombre de la tabla cursos de la base de datos

--- src/main/java/com/anderson/forohub/controller/CursoController.java
+++ src/main/java/com/anderson/forohub/controller/CursoController.java
@@ -1,6 +1,11 @@
 package com.anderson.forohub.controller;
 
+import com.anderson.forohub.domain.curso.DatosCurso;
+import com.anderson.forohub.service.CursoService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("cursos")
 public class CursoController {
 
-//    @PostMapping
+    private CursoService cursoService;
+
+    public CursoController(CursoService cursoService) {
+        this.cursoService = cursoService;
+    }
+
+    @PostMapping
+    public void registrarCurso(@Valid @RequestBody DatosCurso datosCurso){
+    }
 
 }
--- src/main/java/com/anderson/forohub/domain/curso/DatosCurso.java
+++ src/main/java/com/anderson/forohub/domain/curso/DatosCurso.java
@@ -1,9 +1,11 @@
 package com.anderson.forohub.domain.curso;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record DatosCurso(
 
+        @NotBlank
         @Pattern(regexp = "^.{0,200}$", message = "{curso.largo}")
         String nombre
 ) {
