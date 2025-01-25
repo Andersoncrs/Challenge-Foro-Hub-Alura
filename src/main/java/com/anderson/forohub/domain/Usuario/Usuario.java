a3234e4f3cc60c1e1f50c9865c6beeabc15a8daa
Anderson Rodriguez <andersoncamilo.rodriguez.s@gmail.com>
2025-01-25 14:00:58 -0500

    Implementa la clase UserDetails en la clase Usuario.java

--- src/main/java/com/anderson/forohub/domain/Usuario/Usuario.java
+++ src/main/java/com/anderson/forohub/domain/Usuario/Usuario.java
@@ -5,6 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Entity
@@ -12,10 +18,25 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
 @AllArgsConstructor
-public class Usuario{
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String nombreUsuario;
     String Clave;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.Clave;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.nombreUsuario;
+    }
 }
