96a2891d51fde0345e8d4a0712908965218c6184
Anderson Rodriguez <andersoncamilo.rodriguez.s@gmail.com>
2025-01-25 14:08:10 -0500

    Agrega el servicio de Autenticacion de UserDetailsService

--- src/main/java/com/anderson/forohub/domain/Usuario/UsuarioRepository.java
+++ src/main/java/com/anderson/forohub/domain/Usuario/UsuarioRepository.java
@@ -3,6 +3,6 @@ package com.anderson.forohub.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public interface UsuarioInterface extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     UserDetails findByLogin(String nombreUsuario);
 }
---
+++ src/main/java/com/anderson/forohub/infra/security/AtenticationService.java
@@ -0,0 +1,22 @@
+package com.anderson.forohub.infra.security;
+
+import com.anderson.forohub.domain.Usuario.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AtenticationService implements UserDetailsService {
+
+    private UsuarioRepository usuarioRepository;
+
+    public AtenticationService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByLogin(username);
+    }
+}
