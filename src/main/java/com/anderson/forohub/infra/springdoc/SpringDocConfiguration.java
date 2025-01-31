eda495466ac2874bf0a4767ba712149febbf6602
Anderson Rodriguez <andersoncamilo.rodriguez.s@gmail.com>
2025-01-31 10:00:50 -0500

    Personaliza la Documentacion de OpenAPI

--- src/main/java/com/anderson/forohub/infra/springdoc/SpringDocConfiguration.java
+++ src/main/java/com/anderson/forohub/infra/springdoc/SpringDocConfiguration.java
@@ -1,10 +1,17 @@
 package com.anderson.forohub.infra.springdoc;
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SpringDocConfiguration {
@@ -17,6 +24,21 @@ public class SpringDocConfiguration {
                                 new SecurityScheme()
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
-                                        .bearerFormat("JWT")));
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Foro Hub API")
+                        .description("API Rest de la aplicación Foro Hub. Contiene las funcionalidades para Listar, Crear, Modificar y Eliminar Tópicos y Cursos."+
+                                "Implementa como método de Autenticación y Autorización los JWT Tokens")
+                        .contact(new Contact()
+                                .name("Anderson Camilo Rodríguez Salvador")
+                                .email("andersoncamilo.rodriguez.s@gmail.com"))
+                        .version("1.0.0")
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://github.com/Andersoncrs/Challenge-Foro-Hub-Alura/blob/main/LICENSE")))
+                .servers(List.of(new Server().url("http://localhost:8080").description("Servidor Local")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Enlace de GitHub del Proyecto")
+                        .url("https://github.com/Andersoncrs/Challenge-Foro-Hub-Alura"));
     }
 }
